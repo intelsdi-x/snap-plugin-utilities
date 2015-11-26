f3908ace23e67c299e97054b108f068d45788ca4
Marcin Krolik <marcin.krolik@intel.com>
2015-11-26 12:22:39 +0100

    pulse2snap

--- config/config_test.go
+++ config/config_test.go
@@ -24,9 +24,9 @@ package config
 import (
 	"testing"
 
-	"github.com/intelsdi-x/pulse/control/plugin"
-	"github.com/intelsdi-x/pulse/core/cdata"
-	"github.com/intelsdi-x/pulse/core/ctypes"
+	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/core/cdata"
+	"github.com/intelsdi-x/snap/core/ctypes"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
