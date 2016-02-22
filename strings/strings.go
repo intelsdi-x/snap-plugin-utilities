e67f4dc8ac1347a64d3b430e5c8e3a9f171e63d4
Marcin Krolik <marcin.krolik@intel.com>
2016-02-22 10:38:37 +0100

    Simple StringSet implemented

--- strings/strings.go
+++ strings/strings.go
@@ -73,3 +73,39 @@ func All(slice []string, handler func(string) bool) bool {
 	return true
 }
 
+// StringSet is set implementation on top of map
+type StringSet struct {
+	set map[string]bool
+}
+
+// Add adds new item to set
+func (set *StringSet) Add(element string) bool {
+	_, found := set.set[element]
+	set.set[element] = true
+	return !found
+}
+
+// Delete removes element from set
+func (set *StringSet) Delete(element string) bool {
+	_, found := set.set[element]
+	if found {
+		delete(set.set, element)
+	}
+	return found
+}
+
+// Elements returns list of set elements
+func (set *StringSet) Elements() []string {
+	iter := []string{}
+	for k := range set.set {
+		iter = append(iter, k)
+	}
+	return iter
+}
+
+// Init initializes sets internal map
+func Init() StringSet {
+	set := StringSet{}
+	set.set = map[string]bool{}
+	return set
+}
